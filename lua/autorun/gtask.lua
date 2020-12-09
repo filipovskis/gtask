292f99bab16aa935bb9d6e5e5af470a3891589c5
Aleksandrs <tochnonement@gmail.com>
2020-12-09 17:27:21 +0200

    Some aspects are reworked, new functions

--- lua/autorun/gtask.lua
+++ lua/autorun/gtask.lua
@@ -10,7 +10,7 @@ Steam: steamcommunity.com/profiles/76561198086200873
 
 --]]
 
-local remove, CurTime, unpack, isstring, isnumber, isfunction = table.remove, CurTime, unpack, isstring, isnumber, isfunction
+local CurTime, remove, ipairs, unpack = CurTime, table.remove, ipairs, unpack
 
 local task = {}
 local stored = {}
@@ -19,110 +19,164 @@ local function NewTask(data)
     local index = #stored + 1
 
     data.started = CurTime()
+    data.paused = false
     data.index = index
 
     stored[index] = data
+
+    return index
 end
 
 local function CallTask(index)
-    local curtime = CurTime()
     local data = stored[index]
-    local time = data.time
-    local started = data.started
-    local difference = curtime - started
-    local repeats = data.repeats or 1
-    local done = false
+    if data then
 
-    if difference >= time then
-        if not data.infinite then
-            if repeats > 0 then
-                data.started = curtime
-                repeats = repeats - 1
+        if data.paused then return end
+
+        local now = CurTime()
+        local diff = now - data.started
+
+        if diff >= data.time then
+            local repeats = data.repeats
+
+            data.func( unpack(data.args) )
+
+            if not data.infinite then
+                if repeats == 1 then
+                    remove(stored, index)
+                    return true
+                end
+
+                data.repeats = repeats - 1
             end
 
-            if repeats < 1 then
-                done = true
-            end
-        else
-            data.started = curtime
+            data.started = now
         end
-
-        data.func( unpack(data.args) )
-
-        if done then
-            remove(stored, index)
-        end
-
-        data.repeats = repeats
-
-        return true
     end
-
-    return false
 end
 
---- Create a simple task
+--- Create simple task
 ---@param time number
 ---@param func function
 function task.Simple(time, func, ...)
-    assert(time, "<time> cannot be empty")
-    assert(func, "<func> cannot be empty")
-    assert(isnumber(time), "<time> should be a number")
-    assert(isfunction(func), "<func> must be a function")
-
-    NewTask({
+    return NewTask({
         time = time,
         func = func,
-        args = {...}
+        args = {...},
+        repeats = 1
     })
 end
 
---- Create a task with identifier
---- Feature to set repetitions
+--- Create advanced task
 ---@param name string
 ---@param time number
 ---@param repeats number
 ---@param func function
 function task.Create(name, time, repeats, func, ...)
-    assert(name, "<name> cannot be empty")
-    assert(time, "<time> cannot be empty")
-    assert(func, "<func> cannot be empty")
-    assert(isstring(name), "<name> should be a string")
-    assert(isnumber(time), "<time> should be a number")
-    assert(isfunction(func), "<func> must be a function")
-
-    NewTask({
+    local infinite = (repeats == 0)
+        
+    return NewTask({
+        name = name,
         time = time,
         func = func,
+        args = {...},
         repeats = repeats,
-        infinite = (repeats == 0),
-        name = name,
-        args = {...}
+        infinite = infinite
     })
 end
 
---- Remove task by identifier
+--- Get task's data by its name
 ---@param name string
-function task.Remove(name)
-    local timer_name
+---@return table
+function task.Get(name)
     for index, data in ipairs(stored) do
-        timer_name = data.name or ""
-        if (timer_name == name) then
-            remove(stored, index)
+        if (data.name == name) then
+            return data, index
         end
     end
 end
 
---- Return all tasks
+--- Get all tasks
 ---@return table
 function task.GetTable()
     return stored
 end
 
-hook.Add("Think", "Task.Think", function()
-    for index in ipairs(stored) do
+--- Check if task with given name exists
+---@param name string
+---@return boolean
+function task.Exists(name)
+    return task.Get(name) ~= nil
+end
+
+--- Delete task
+--- *Alias: task.Remove*
+---@param name string
+function task.Kill(name)
+    local index = select(2, task.Get(name))
+
+    remove(stored, index)
+end
+
+--- Get how many repetitions left
+---@param name string
+---@return number
+function task.RepsLeft(name)
+    local obj = task.Get(name)
+
+    return obj.repeats
+end
+
+--- Get how much **not rounded** time left
+---@param name string
+---@return number
+function task.TimeLeft(name)
+    local obj = task.Get(name)
+    local diff = (CurTime() - obj.started)
+
+    return obj.time - diff
+end
+
+--- Pause or unpause task
+---@param name string
+---@param bool boolean
+function task.Pause(name, bool)
+    local obj = task.Get(name)
+    
+    obj.paused = bool
+end
+
+--- Toggle task (pause or unpause)
+---@param name string
+function task.Toggle(name)
+    local obj = task.Get(name)
+    
+    obj.paused = not obj.paused
+end
+
+--- Adjust task's delay
+---@param name string
+---@param time number
+function task.Adjust(name, time)
+    local obj = task.Get(name)
+
+    obj.time = time
+    obj.started = CurTime()
+end
+
+--- Do one repeat for task
+---@param name string
+function task.Complete(name)
+    local obj = task.Get(name)
+    obj.time = 0
+end
+
+task.Remove = task.Kill
+
+hook.Add("Tick", "gtask.Tick", function()
+    for index = 1, #stored do
         CallTask(index)
     end
 end)
 
-_G["task"] = task
+_G.task = task
